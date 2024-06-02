9ca0c56178b2b801a80310245e99abbb20dda214
Daniel Krot <danielkr1324@gmail.com>
2024-06-02 14:26:34 +0300

    added login/logout funcitonality with clerk auth provider

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,20 @@
+'use client'
+
+import { SignOutButton } from "@clerk/nextjs"
+import { useToast } from "../ui/use-toast"
 
 function SignOutLink() {
+  const {toast} = useToast()
+
+  const handleLoguot = () => {
+    toast({description: 'You have been signed out'})
+  }
   return (
-    <div>SignOutLink</div>
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left" onClick={handleLoguot}>
+        Logout
+      </button>
+    </SignOutButton>
   )
 }
 
